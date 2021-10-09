a036e5ce06ed9d143d950b26e85a8de0dfc75fc5
alifakbarr <alifakbar8800@gmail.com>
2021-10-09 13:32:06 +0700

    create user data'

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Models\User;
+use Illuminate\Validation\Rules\Unique;
+
+class RegisterController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(RegisterRequest $request)
+    {
+        // return 'register'; //test connect
+
+
+        User::create([
+            'name' => request('name'),
+            'username' => request('username'),
+            'email' => request('email'),
+            'password' => bcrypt(request('password')),
+        ]);
+
+        return response('Thanks register');
+    }
+}
